a24987f20790bb304fd30fd416117935bc3de7fb
dominik59 <dominik-pawlik1@wp.pl>
2015-12-09 14:01:35 +0100

    dodane trzecie okno i wiecej przejsc

--- Creepy_Squirells/src/Core/Resources.java
+++ Creepy_Squirells/src/Core/Resources.java
@@ -1,5 +1,23 @@
 package Core;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.Sound;
+
 
 public class Resources {
-
+	private static Map<String, Image> images;
+	private static Map<String, SpriteSheet> spritesheets;
+	private static Map<String, Sound> sounds;
+	
+	public Resources()
+	{
+		images = new HashMap<String, Image>();
+		spritesheets = new HashMap<String, SpriteSheet>();
+		sounds= new HashMap<String,Sound>();
+		
+	}
+	
 }
